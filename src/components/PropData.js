2d8e5e18305b35a8020d2f5ba16569c03f5f75cf
Anson Chung <ansonchung3@gmail.com>
2023-07-04 21:34:17 +0100

    Added test for using cache data from js file

---
+++ src/components/PropData.js
@@ -0,0 +1,2 @@
+export const numData = 6;
+export const arrayData = [1, 2, 3];
--- src/pages/PlaceHolderPage.vue
+++ src/pages/PlaceHolderPage.vue
@@ -1,5 +1,27 @@
 <template>
     <div>
         <h1>Place Holder - Stock Exchange</h1>
+        <p>{{ test }}</p>
+        <p>{{ array }}</p>
     </div>
 </template>
+
+<script>
+import * as data from 'src/components/PropData.js';
+
+export default {
+    data () {
+        return {
+            array: data.arrayData
+        }
+    },
+    computed: {
+        test () {
+            return data.numData + 1;
+        }
+        // array () {
+        //     return data.arrayData;
+        // }
+    }
+};
+</script>
