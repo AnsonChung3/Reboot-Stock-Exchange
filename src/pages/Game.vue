a73bb99b1a1bd71d14d3872100418d52f752f9f6
Anson Chung <ansonchung3@gmail.com>
2024-03-18 17:19:52 +0000

    New component

---
+++ src/pages/Game.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Game.vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    created () {
+        if (localStorage.getItem('gameData') === null) {
+            console.log('datad not found, call to init data');
+        }
+    }
+}
+</script>
