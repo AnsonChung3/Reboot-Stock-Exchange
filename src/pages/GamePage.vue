43a075174acafbacc0483b4d5fd9297dde035f07
Anson Chung <ansonchung3@gmail.com>
2024-03-19 19:11:58 +0000

    Removed comments

--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -23,10 +23,6 @@ export default {
     watch: {
         currentCycle (newValue, oldValue) {
             if (newValue === this.lastPlayCycle - 1) {
-                // at the moment last cycle is 5
-                // when newValue hits 5, before the display cards updates,
-                // watcher is triggered
-                // display cards only updates after the alert is dismissed
                 alert('the next cycle is the last playable');
             } else if (newValue === this.lastPlayCycle) {
                 alert('this is the end of game, showing end screen next');
