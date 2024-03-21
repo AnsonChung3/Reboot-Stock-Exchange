d541dbe5075aa42f2bd0a3f125de021162f191b0
Anson Chung <ansonchung3@gmail.com>
2024-03-21 15:19:31 +0000

    Setting up buying in display card

--- src/components/displayCard.vue
+++ src/components/displayCard.vue
@@ -5,6 +5,18 @@
         <div>Prices: {{ stock.prices }}</div>
         <div>Start Cycle: {{ startCycle }}</div>
         <div>Current Cycle: {{ currentCycle }}</div>
+        <q-input
+            v-model="tradeQty"
+            type="number"
+            min=0
+            placeholder="Trade Quantity"
+            dark
+        />
+        <q-btn
+            label="buy"
+            @click="buy"
+            outline
+        />
         <hr>
     </div>
 </template>
@@ -13,6 +25,11 @@
 export default {
     props: {
         stock: Object
+    },
+    data () {
+        return {
+            tradeQty: undefined
+        };
     },
     computed: {
         startCycle () {
@@ -27,6 +44,16 @@ export default {
         },
         currentCycle () {
             return this.startCycle + this.$store.state.game.gameCycle;
+        }
+    },
+    methods: {
+        buy () {
+            if (this.tradeQty <= 0 || this.tradeQty === undefined) {
+                alert("Please input value greater than zero");
+                return;
+            }
+            // btn disabled if not enough funding
+            console.log(`trade qty is ${this.tradeQty}, price is ${this.stock.prices[this.currentCycle]}`);
         }
     }
 }
