56b31d6f4406a881a26641fadde95590c1ed59df
Anson Chung <ansonchung3@gmail.com>
2024-03-22 23:05:38 +0000

    Remove debug console logs

--- src/store/game/actions.js
+++ src/store/game/actions.js
@@ -34,8 +34,6 @@ export function trade ({ state, commit }, payload) {
 }
 
 export async function initGameData () {
-    // console.log('action - init game data');
-
     // response 1 is the unhandled raw response
     const response1 = await axios.get('http://api.marketstack.com/v1/tickers',
     {
@@ -44,16 +42,11 @@ export async function initGameData () {
         }
     });
 
-    // console.log('request for tickers success');
-
     const gameData = [];
     // response one get's handled here
     for (let i = 0; i <= 3; i++) {
         gameData.push(helpers.trimObj(response1.data.data[i]));
     };
-
-    // console.log('end of for loop, print game data');
-    // console.log(gameData);
 
     const promises = [];
     // IT USES 2 API REQUETS IN ONE GO
@@ -67,20 +60,15 @@ export async function initGameData () {
                 }
             )
                 .then(response => {
-                    // console.log('request for individual price success');
                     gameData[i].prices = helpers.mapPrices(response.data.data);
-                    // console.log(gameData);
                 })
         )
     }
 
     Promise.all(promises).then(() => {
-            // console.log('promises.all');
-            // console.log(gameData);
             // local storage set item
             localStorage.setItem('game_data', JSON.stringify(gameData));
             // call mutation, and populate store from local storage
         }
     )
-    // console.log('end of action - init game');
 }
--- src/store/game/mutations.js
+++ src/store/game/mutations.js
@@ -18,11 +18,7 @@ export function mutateFunding (state, payload) {
 }
 
 export function initGameMarket (state, payload) {
-    // console.log('commit init game market');
-    // console.log(state.marketData);
     for (let i = 0; i < payload.data.length; i++) {
         state.marketData.push(payload.data[i]);
     }
-    // console.log('end of for loop to add data to market');
-    // console.log(state.marketData);
 }
