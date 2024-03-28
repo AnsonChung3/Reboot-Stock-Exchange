c27d3730bf1d2d165fdd95dc308f7f7a81fd6ed3
Anson Chung <ansonchung3@gmail.com>
2024-03-28 18:16:24 +0000

    Added comment

--- src/store/game/state.js
+++ src/store/game/state.js
@@ -3,6 +3,7 @@ export default function () {
     test: 90,
     marketData: [],
     playerAccount: {
+        // funding gets times 100 to be align with price data, force integer
         funding: 100000 * 100,
         holdings: []
     },
