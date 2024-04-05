52d199a8d8a964276d8bfa213d4a76f82851afd1
Anson Chung <ansonchung3@gmail.com>
2024-04-05 16:39:43 +0100

    Added "buy max" and imported confirmation dialogue

--- src/components/displayBuy.vue
+++ src/components/displayBuy.vue
@@ -1,44 +1,50 @@
 <template>
-    <div class="display-buy">
-        <div class="stock-name">{{ stock.name }}</div>
-        <div>Symbol: {{ stock.symbol }}</div>
-        <div>Prices: {{ currentPrice/100 }}</div>
-        <div class=row>
-            <q-input
-                class="col-5"
-                v-model="tradeQty"
-                type="number"
-                placeholder="Trade Quantity"
-                dark
-            />
-            <trade-btn
-                class="col"
-                labelTxt="buy"
-                :disableCondition="!allowTrade"
-                @click=buy
-            />
-            <trade-btn
-                class="col"
-                labelTxt="buy max"
-                @click=test
-            />
+    <div>
+        <confirm-modal v-model="requestConfirm" @confirmTrade="buyMax"/>
+        <div class="display-buy">
+            <div class="stock-name">{{ stock.name }}</div>
+            <div>Symbol: {{ stock.symbol }}</div>
+            <div>Prices: {{ currentPrice/100 }}</div>
+            <div class=row>
+                <q-input
+                    class="col-5"
+                    v-model="tradeQty"
+                    type="number"
+                    placeholder="Trade Quantity"
+                    dark
+                />
+                <trade-btn
+                    class="col"
+                    labelTxt="buy"
+                    :disableCondition="!allowTrade"
+                    @click=buy
+                />
+                <trade-btn
+                    class="col"
+                    labelTxt="buy max"
+                    @click="requestConfirm = !requestConfirm"
+                />
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import TradeBtn from './tradeBtn.vue';
+import ConfirmModal from './confirmModal.vue';
 
 export default {
     props: {
         stock: Object
     },
     components: {
-        TradeBtn
+        TradeBtn,
+        ConfirmModal
     },
     data () {
         return {
-            tradeQty: undefined
+            tradeQty: undefined,
+            requestConfirm: false
         };
     },
     computed: {
@@ -59,8 +65,9 @@ export default {
         }
     },
     methods: {
-        test () {
-            console.log('test');
+        buyMax () {
+            this.tradeQty = Math.floor(this.funding / this.currentPrice);
+            this.buy();
         },
         buy () {
             this.$store.dispatch('game/trade', {
