3fd32f4dbf141d45f899630119ee2eca63683681
Anson Chung <ansonchung3@gmail.com>
2024-04-10 16:49:33 +0100

    CSS styling tweak and trim bulk. Added comment for extra info

--- src/components/displayEndGame.vue
+++ src/components/displayEndGame.vue
@@ -1,37 +1,48 @@
 <template>
     <div>
         <h2>End of game Report</h2>
+        <div class="display-block">
             <div>Current Fund: {{ displayFunding }}</div>
             <div>Holdings value: {{ displayValHolding }}</div>
-            <hr>
-            <div>Total Value: {{ displayTotalValue }}</div>
+        </div>
+        <hr>
+        <div class="display-block">
+            <div>The combined value for your current funding and stock is {{ displayTotalValue }}</div>
             <div>Your starting fund was 100000.00</div>
-            <div>You have earned/lost [ this much money ]</div>
-            <q-btn
-                label="press to start"
-                @click="emitRestart"
-                outline
-            />
+            <div>{{ reportText }}</div>
+        </div>
+        <q-btn
+            label="press to start"
+            @click="emitRestart"
+            outline
+        />
     </div>
 </template>
 
 <script>
 export default {
     computed: {
-        funding () {
-            return this.$store.state.game.playerAccount.funding;
+        totalAccountValue () {
+            // kept for code readability
+            return this.$store.getters['game/getTotalHoldingValue'] + this.$store.state.game.playerAccount.funding;
         },
+        // all 4 properties below return a string
         displayFunding () {
-            return (this.funding / 100).toFixed(2);
+            return (this.$store.state.game.playerAccount.funding / 100).toFixed(2);
         },
         displayValHolding () {
-            // returns a string for display
             const val = this.$store.getters['game/getTotalHoldingValue'];
             return (val / 100).toFixed(2);
         },
         displayTotalValue () {
-            const val = this.$store.getters['game/getTotalHoldingValue'] + this.funding;
-            return (val / 100).toFixed(2);
+            return (this.totalAccountValue / 100).toFixed(2);
+        },
+        reportText () {
+            const difference = this.totalAccountValue - (100000 * 100);
+            const val = Math.abs(difference / 100);
+            const displayVal = val.toFixed(2);
+            const text = (difference >= 0) ? 'earned' : 'lost';
+            return `You have ${text} $${displayVal}`;
         }
     },
     methods: {
@@ -41,3 +52,9 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.display-block {
+    margin: 2% 0% 2% 0%
+}
+</style>
