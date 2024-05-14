459f1069926d0099a3ffbd5d8d04d2bb2f951d9c
Anson Chung <ansonchung3@gmail.com>
2024-05-14 20:50:33 +0100

    Added more  descriptives to displayIntro. Added button and input field for players' API key

--- src/components/displayIntro.vue
+++ src/components/displayIntro.vue
@@ -1,24 +1,106 @@
 <template>
     <div>
         <h2>Introduction</h2>
-        <div>
+        <div v-if="page === 0">
             <p>Thank you for trying out my personal project!</p>
             <p>You will be shown prices pulled from external API with real market data.</p>
             <p>The goal is to profit as much as possible within 14 trade days.</p>
+            <br>
+            <q-btn
+                label="next"
+                @click="page++"
+                outline
+            />
         </div>
-        <q-btn
-            label="Start trading"
-            @click="emitStart"
-            outline
-        />
+        <div v-if="page === 1 && !hasData">
+            <p>Before you start, please go to <a :href="url">marketstack</a>.</p>
+            <p>You can register for a free account for an API key. It is necessary for procceding the game.</p>
+            <p>Put your API key in the input box below:</p>
+            <q-input
+                type="text"
+                v-model="customerKey"
+                outlined
+                dark
+            />
+            <q-btn
+                label="next"
+                @click="addAPIKey"
+                outline
+            />
+        </div>
+        <div v-if="page === 1 && hasData">
+            <p>There are already data stored to your browser. You can start game with existing data or fetch new data.</p>
+            <p><b>Note: data from the API are end of day data. Fetching new data in the same day won't give you different results. Each initiation of data will cost 11 API requests.</b></p>
+            <q-btn
+                label="fetch new"
+                @click="hasData = !hasData"
+            />
+            <q-btn
+                label="Start trading"
+                @click="emitStart"
+                outline
+            />
+        </div>
+        <div v-if="page === 2">
+            <p>The game will fetch the price data for a total of 10 stocks.</p>
+            <p>4 stocks will be randomly chosen for each game.</p>
+            <p>The returned data from the API will be stored in the local storage. You can see it under the "Application" tab if you go to your browser dev tools.</p>
+            <p>Time for a little game!</p>
+            <q-btn
+                label="Start trading"
+                @click="emitStart"
+                outline
+            />
+        </div>
     </div>
 </template>
 
 <script>
+import { axios } from 'boot/axios.js';
+
 export default {
+    data() {
+        return {
+            page: 0,
+            url: 'https://marketstack.com/',
+            customerKey: undefined,
+            hasData: false
+        }
+    },
     methods: {
         emitStart () {
             this.$emit('startGame');
+        },
+        async addAPIKey () {
+            const key = this.customerKey;
+            if (key === undefined || key === '') {
+                alert('Please input an API key to continue.');
+                return
+            }
+            console.log(key);
+            const validKey = await axios.get('http://api.marketstack.com/v1/eod',
+                {
+                    params: {
+                        access_key: key,
+                        symbols: 'AAPL'
+                    }
+                }
+            )
+            .catch(error => {
+                this.display = error;
+                console.log('uh oh');
+                return false;
+            })
+            if (validKey === false) {
+                alert(`Please make sure you put the correct API key.`);
+                return;
+            }
+            this.page++;
+        }
+    },
+    created () {
+        if (localStorage.getItem('game_data') !== null) {
+            this.hasData = true;
         }
     }
 };
