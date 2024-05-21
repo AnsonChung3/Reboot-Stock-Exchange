86f41d8e29687470a838a2f596c6b140624c7a24
Anson Chung <ansonchung3@gmail.com>
2024-05-21 19:27:25 +0100

    Added new test

--- src/pages/testPage.vue
+++ src/pages/testPage.vue
@@ -3,8 +3,8 @@
     <div>{{ display }}</div>
     <div>
         <q-btn
-            label=test
-            @click="testReq"
+            label="flick switch"
+            @click="flickSwitch"
             outline
         />
         <q-btn
@@ -18,41 +18,21 @@
 </template>
 
 <script>
-import { axios } from 'boot/axios.js';
-import { APIKEY } from 'components/APIKEY.js';
+// import { axios } from 'boot/axios.js';
+// import { APIKEY } from 'components/APIKEY.js';
 // import * as helpers from 'components/helpers.js'
 
 export default {
     data () {
         return {
-            display: 'text'
+            display: 'text',
+            var: false
         }
     },
     methods: {
-        testReq () {
-            const array = ['AAPL', 'AL', 'MSFT'];
-            const promises = [];
-            for (let i = 1; i < array.length; i++) {
-                promises.push(
-                    axios.get('http://api.marketstack.com/v1/eod',
-                        {
-                            params: {
-                                access_key: APIKEY,
-                                symbols: array[i]
-                            }
-                        }
-                    )
-                    .then(response => {
-                        console.log(response);
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    })
-                )
-            }
-            Promise.all(promises).then(() => {
-                console.log('all requests resolved, store to local storage next');
-            });
+        flickSwitch () {
+            this.display = this.var;
+            this.var = !this.var;
         },
         clearStorage () {
             localStorage.removeItem('game_data');
